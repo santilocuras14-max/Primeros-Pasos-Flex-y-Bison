158e9040281fe6f417b5a417b5eff23b9f57822d
santilocuras14-max <santilocuras14@gmail.com>
2025-08-19 21:37:04 -0500

    Add files via upload

--- Ejemplos/ex1-4_hand_scanner.c
+++ Ejemplos/ex1-4_hand_scanner.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <ctype.h>
 
+int main(void){
+    int c;
+    while((c = getchar()) != EOF){
+        if(isspace(c)){
+            if(c=='\n') puts("EOL");
+            continue;
+        } else if(isdigit(c)){
+            long v = c - '0';
+            while((c = getchar()) != EOF && isdigit(c)){
+                v = v*10 + (c - '0');
+            }
+            printf("NUMBER(%ld)\n", v);
+            if(c==EOF) break;
+            ungetc(c, stdin);
+        } else {
+            switch(c){
+                case '+': puts("PLUS"); break;
+                case '-': puts("MINUS"); break;
+                case '*': puts("MUL"); break;
+                case '/': puts("DIV"); break;
+                case '(': puts("OP"); break;
+                case ')': puts("CP"); break;
+                case '|': puts("ABS_OR_BAR"); break;
+                default:  printf("MYSTERY(%c)\n", c); break;
+            }
+        }
+    }
+    return 0;
+}
